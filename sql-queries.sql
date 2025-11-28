26ca49c138637dbfad9cadc797d148b19eb8787a
drewe4444 <duru.elli123@gmail.com>
2025-11-28 15:53:38 -0500

    Create sql-queries.sql
    Signed-off-by: drewe4444 <duru.elli123@gmail.com>

---
+++ sql-queries.sql
@@ -0,0 +1,10 @@
+-- Retrieve all projects located in a specific province (e.g., 'Ontario').
+SELECT * FROM Projects WHERE
+
+-- List all projects with a status of 'In-Progress'.
+
+-- Count the number of projects assigned to each contractor.
+
+-- Update the status of a specific project to 'Completed'.
+
+-- Calculate the total budget of all projects, grouped by province.
