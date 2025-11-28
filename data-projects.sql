78dc3fe596e1cc0619f2ed862f8d6698a6cacbc2
drewe4444 <duru.elli123@gmail.com>
2025-11-28 14:05:15 -0500

    Update data-projects.sql
    deleted projects_id, it is auto_increment thus unnecessary
    
    Signed-off-by: drewe4444 <duru.elli123@gmail.com>

--- data-projects.sql
+++ data-projects.sql
@@ -1,5 +1,5 @@
 USE national_infrastructure;
 
-INSERT INTO Projects (project_id, project_name, project_management, project_status, budget, winning_bidder, project_hq) VALUES
+INSERT INTO Projects (project_name, project_management, project_status, budget, winning_bidder, project_hq) VALUES
 ( ),
 ( );
