372060e01a56377c3673abdc802bd3c9a7accb45
drewe4444 <duru.elli123@gmail.com>
2025-11-28 14:05:34 -0500

    Update data-provinces.sql
    deleted province_id, it is auto_increment thus unnecessary
    
    Signed-off-by: drewe4444 <duru.elli123@gmail.com>

--- data-provinces.sql
+++ data-provinces.sql
@@ -1,5 +1,5 @@
 USE national_infrastructure;
 
-INSERT INTO Provinces (province_id, province_name, indigenous_land_overlap, requires_environmental_review, environmental_grade, construction_start, construction_end, median_labor_cost, median_material_cost, infrastructure_ministry_contact, regional_office) VALUES
+INSERT INTO Provinces (province_name, indigenous_land_overlap, requires_environmental_review, environmental_grade, construction_start, construction_end, median_labor_cost, median_material_cost, infrastructure_ministry_contact, regional_office) VALUES
 ( ),
 ( );
