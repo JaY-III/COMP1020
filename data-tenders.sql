ae1aed3096896a78e554c95e747ff4f3637e4cc3
drewe4444 <duru.elli123@gmail.com>
2025-11-28 10:29:24 -0500

    Create data-tenders.sql
    Signed-off-by: drewe4444 <duru.elli123@gmail.com>

---
+++ data-tenders.sql
@@ -0,0 +1,5 @@
+USE national_infrastructure;
+
+INSERT INTO Tenders (tender_id, tender_name, contractor_id, project_id, province_id, bid_value, bid_status, bid_opening_date, bid_submission_deadline) VALUES
+( ),
+( );
