e3787697e02809587934aa6f33e63c3154fecff8
nanaaishatmohammed66 <nanaaishatmohammed66@gmail.com>
2025-11-29 11:10:16 -0500

    Update data-contractors.sql
    Signed-off-by: nanaaishatmohammed66 <nanaaishatmohammed66@gmail.com>

--- data-contractors.sql
+++ data-contractors.sql
@@ -1,5 +1,22 @@
 USE national_infrastructure;
 
 INSERT INTO Contractors (company_name, phone_number, email_address, street_num, street_name, city) VALUES
-( ),
-( );
+('ABC construction','(+1)123-4567','contactabccontruction@gmail.com','555' 'springfield street' 'Brampton' ),
+('Elite construction','(+1)457-4990','contactelitecontruction@gmail.com','234' 'Maple street' 'calagry' ),
+ ('summit construction','(+1)346-2367','officesummitcontruction@gmail.com','987' 'spurse street' 'edmonton' ),
+('Apex Renovations','(+1)890-5672','buildwithapexn@gmail.com','555' 'springfield street' 'Brampton'),
+('Mastercraft construction','(+1)732-6233','projectsmastercrafts@gmail.com','679' 'Magolia street' 'Markham' ),
+('Probuild groups','(+1)895-4107','probuildcontruction@gmail.com','268' 'hawthrone' 'Vaughan' ),
+('Maple leaf contruction','(+1)760-2234','contactmapleleafcontractors@gmail.com','290' 'Balsam street' 'saskatoon' ),
+('Canadian Heritage Builders','(+1)215-4568','build@canadianheritage.com','291' 'laurel Dr ' 'saskatoon' ),
+('Green Canada Builders','(+1)294-8901','contactgreencanada@gmail.com','789' 'makreal street' 'Markham' ),
+('Boreals painting co.','(+1)894-4450','contactborealspaint@gmail.com','540' 'Dogwood street' 'Victoria' ),
+('Tundra Tile & Stone','(+1)891-8506','tundratile@gmail.com','678' 'Chestnut Ave' 'Regina' ),
+('sheild Contracting Limited','(+1)227-6572','sheildcontractors@gmail.com','298' 'Polpular Ave' 'London' ),
+('St. Lawrence Structures ','(+1)445-8965','contactslcstructors@gmail.com','231' 'Country Club Dr' 'Kingston' ),
+('Great Northern construction','(+1)120-3324','contactgreatnorthcontruction@gmail.com','685' 'Locust Street' 'Vaughan' ),
+('Dominion Builders ','(+1)123-4569','dominionbuilders@gmail.com','346' 'Juniper street' 'Hamilton' ),
+('EcoNorth Renovations','(+1)435-4501','contacteconorthrenovations@gmail.com','unit 260' 'Aspen Rd' 'London' ),
+('Drew Deck & Contruction',(+1)437-8509',drewdeckcanada@gmail.com','unit 236','Queen Street', 'Kington'),
+('Jordan Contruction',(+1)431-8556',contactjordancontruction@gmail.com','unit 458','Princess Street', 'Kington')
+('Polar Doors & Windows,'(+1)341-2144','service@polar.com','257','Mervile Rd','Otatwa');
